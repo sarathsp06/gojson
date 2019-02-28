698fe6a4a43d49a79d01d63a82f24a324e0abbc3
sarathsp06 <sarath.sp06@gmail.com>
2019-02-28 17:02:31 +0100

    Initial commit - MVP
    Basic functionalities
    The tool can read json from stdin
    It can print json objects to any depth, print array elements

---
+++ README.md
@@ -0,0 +1,19 @@
+Sample usage 
+-------------
+Install
+```
+go get github.com/sarathsp06/gojson
+```
+
+
+
+
+
+Usage
+```
+cat test.json| go run main.go glossary.titles                                                                             \
+cat test.json| go run main.go glossary
+cat test.json| go run main.go glossary.titles.1
+```
+
+
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/TylerBrock/colorjson"
+	"github.com/pkg/errors"
+
+	"strings"
+)
+
+//getObject returns the object into which the json can be decoded
+func getObject(jsn []byte) (interface{}, bool) {
+	if len(jsn) == 0 {
+		return json.RawMessage{}, false
+	}
+	switch jsn[0] {
+	case '{':
+		return make(map[string]json.RawMessage), true
+	case '[':
+		return []json.RawMessage{}, true
+	case 'n':
+		return json.RawMessage{}, false
+	case '"', '\'':
+		return "", false
+	default:
+		return 0, false
+
+	}
+}
+
+func lookup(key string, data []byte) ([]byte, error) {
+	var v, ok = getObject(data)
+	keys := strings.Split(key, ".")
+	for _, key := range keys {
+		if !ok {
+			return nil, nil
+		}
+		switch v := v.(type) {
+		case map[string]json.RawMessage:
+			if err := json.Unmarshal(data, &v); err != nil {
+				return nil, err
+			}
+			data, ok = v[key]
+			if !ok {
+				return nil, errors.New("invalid key")
+			}
+		case []json.RawMessage:
+			if err := json.Unmarshal(data, &v); err != nil {
+				return nil, err
+			}
+			idx, err := strconv.Atoi(key)
+			if err != nil {
+				return nil, errors.New("invalid key:" + key)
+			}
+			if len(v) < idx || idx < 0 {
+				return nil, errors.New("invalid index:" + key)
+			}
+			data = v[idx]
+		}
+		v, ok = getObject(data)
+	}
+	return data, nil
+}
+
+type formatter interface {
+	Marshal(interface{}) ([]byte, error)
+}
+
+func formatJSON(f formatter, data []byte) ([]byte, error) {
+	v, _ := getObject(data)
+	if err := json.Unmarshal(data, &v); err != nil {
+		return data, err
+	}
+	formattedJSON, err := f.Marshal(v)
+	if err != nil {
+		return data, err
+	}
+	return formattedJSON, nil
+}
+
+func getKey() string {
+	if len(os.Args) == 1 {
+		return ""
+	}
+	key := strings.TrimSpace(os.Args[1])
+	return strings.TrimFunc(key, func(c rune) bool { return c == '.' })
+}
+
+func main() {
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Panicf("reading input error : %v", err)
+	}
+	defer os.Stdin.Close()
+
+	key := getKey()
+	if key != "" {
+		data, err = lookup(key, data)
+		if err != nil {
+			log.Println("Error occurred looking up key . Error : ", err)
+			return
+		}
+	}
+	f := colorjson.NewFormatter()
+	f.Indent = 4
+	formattedJSON, err := formatJSON(f, data)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("%s\n", string(formattedJSON))
+}
